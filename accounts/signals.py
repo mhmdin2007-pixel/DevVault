5dde0d9f2911b55b92b644931c1289ff6215f1b4
mhmdin <mhmdin2007@gmail.com>
2026-07-01 17:50:20 +0330

    Setup Bootstrap and template architecture. adding a path in config/urls.py and editing settings.py for Setup html pages

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_profile(sender, instance:User, created:bool, **kwargs) -> None:
+    '''
+    Automatically create a profile whenever a new user is created.
+    '''
     if created:
         Profile.objects.create(user=instance)
--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
